cb87e27865976ef440e17713efe65632ad74eb5d
Keerikadan03 <fenseraju@gmail.com>
2024-10-07 22:43:35 +0530

    changes in translations

--- src/components/Translations.tsx
+++ src/components/Translations.tsx
@@ -19,9 +19,9 @@ const Translations = () => {
               className="md:basis-1/3 lg:basis-1/5"
               key={translation.title || index}
             >
-              <div className="bg-white rounded-lg shadow my-5 w-64 py-5 mx-auto flex items-center justify-center">
+              <div className="bg-white rounded-lg shadow my-5 w-64 p-5  mx-auto flex items-center justify-center">
                   <div className="space-y-4">
-                    <h3 className="text-lg font-medium min-h-16" title={translation.title}>
+                    <h3 className="text-lg font-medium min-h-20" title={translation.title}>
                       {translation.title.length > 25
                         ? `${translation.title.substring(0, 35)}...`
                         : translation.title}
