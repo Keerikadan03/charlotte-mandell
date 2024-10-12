bb6974f447ccca48c9fd76700573c69289a1d293
Keerikadan03 <fenseraju@gmail.com>
2024-10-12 17:18:48 +0530

    changes in contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,7 @@
 import { ContactForm } from "@/components/ContactForm";
 import banner from "../assets/sections/typing.avif";
 import { Title } from "@/components/typography/Title";
+import Section from "@/components/Section";
 
 const Contact = () => {
   return (
@@ -8,7 +9,9 @@ const Contact = () => {
       <Title title="let's get in touch" description="Say hello! Use the form or details below for any inquiries or collaborations." background={banner} />
       {/* content */}
       <div className="max-w-6xl mx-auto py-6">
+      <Section>
         <ContactForm />
+      </Section>
       </div>
     </div>
   );
